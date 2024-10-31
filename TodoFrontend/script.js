a7ff595b9da104cc7832d97539b40c1a3d04a627
Ryan Mcmanus <35876565+r-j-mcmanus@users.noreply.github.com>
2024-10-31 14:20:44 +0000

    add todos to local storage

--- TodoFrontend/script.js
+++ TodoFrontend/script.js
@@ -1,10 +1,20 @@
 const form = document.querySelector("#todo-form")
 
+let TODOs = []
+
 form.addEventListener("submit", (event) =>
 {
     event.preventDefault(); // strops the browser automatically doing things, this script is wholey responsible
 
     //add todo and render
+    TODOs.push(
+        {
+            title: event.target[0].value,
+            complete: false,
+            id: self.crypto.randomUUID() // after generating 1 billion UUIDs every second for approximately 100 years would the probability of creating a single duplicate reach 50%.
+        }
+    );
+
 
     form.reset(); // puts all input fields back to normal
 });
